379ca8f7d7e39148b4e8ed13fefe69f128a9003b
Israr Khan <israrkdp@gmail.com>
2026-01-09 09:39:53 +0430

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,7 +59,7 @@ export default function Header(){
             <NavLink onClick={() => setOpen(false)} to="/about" className="block text-lg text-slate-100">About</NavLink>
             <NavLink onClick={() => setOpen(false)} to="/projects" className="block text-lg text-slate-100">Projects</NavLink>
             <NavLink onClick={() => setOpen(false)} to="/contact" className="block text-lg text-slate-100">Contact</NavLink>
-            <a href="/israrKhanResume.pdf" target="_blank" rel="noopener noreferrer" className="inline-block mt-4 px-4 py-2 bg-cyan-400 text-slate-900 rounded-md">Resume</a>
+            <a href="/israrKhanResume.pdf" target="_blank" rel="noreferrer"><button className="ml-4 inline-flex items-center px-4 py-2 rounded-md bg-cyan-400 text-slate-900 font-semibold hover:bg-cyan-500 cursor-pointer">Resume</button></a>
           </div>
         </div>
       </div>
